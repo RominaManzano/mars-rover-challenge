43d40c7a2d60f7cb947b05566cfb871c91cfc4c9
Romina Manzano <manzano.romina13@gmail.com>
2023-07-14 19:45:08 -0300

    FIX: cameras constants spacing

--- src/constants/cameras.ts
+++ src/constants/cameras.ts
@@ -8,13 +8,13 @@ export interface RoverCamera {
 enum CameraEnum {
   FHAZ = 'Front Hazard Avoidance Camera',
   RHAZ = 'Rear Hazard Avoidance Camera',
-  MAST =	'Mast Camera',
+  MAST = 'Mast Camera',
   CHEMCAM = 'Chemistry and Camera Complex',
-  MAHLI =	'Mars Hand Lens Imager',
-  MARDI =	'Mars Descent Imager',
-  NAVCAM =	'Navigation Camera',
-  PANCAM =	'Panoramic Camera',
-  MINITES =	'Miniature Thermal Emission Spectrometer(Mini- TES)',
+  MAHLI = 'Mars Hand Lens Imager',
+  MARDI = 'Mars Descent Imager',
+  NAVCAM = 'Navigation Camera',
+  PANCAM = 'Panoramic Camera',
+  MINITES = 'Miniature Thermal Emission Spectrometer(Mini- TES)',
 };
 
 const cameras: { [key: string]: RoverCamera[] } = {
