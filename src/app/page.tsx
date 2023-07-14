5a15ea4ac4767b5b05edebb40d311105bd1241f5
Romina Manzano <manzano.romina13@gmail.com>
2023-07-14 20:29:26 -0300

    UPDATE: general styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import classnames from 'classnames';
+
 import RoverCard from '@/components/RoverCard';
 import { Rover } from '@/types/Rover.type';
 
-const Home: React.FC = () => (
-  <main className="flex min-h-screen flex-col md:flex-row items-center justify-between p-24 gap-8">
-    <RoverCard name={Rover.CURIOSITY} />
-    <RoverCard name={Rover.OPPORTUNITY} />
-    <RoverCard name={Rover.SPIRIT} />
-  </main>
-);
+const Home: React.FC = () => {
+  // const backgroundClasses = 'bg-gradient-to-br from-black via-black to-orange-900';
+  const generalClasses = 'flex min-h-screen flex-col md:flex-row items-center justify-between p-24 gap-8';
+
+  return (
+    <main className={classnames(generalClasses)}>
+      <RoverCard name={Rover.CURIOSITY} />
+      <RoverCard name={Rover.OPPORTUNITY} />
+      <RoverCard name={Rover.SPIRIT} />
+    </main>
+  );
+};
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,17 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    // colors: {
+    //   deepMarsRed: '#8B0000',
+    //   ruestiOrange: '#A0522D',
+    //   indigoBlue: '#2E3842',
+    //   ashenGray: '#808080',
+
+    //   martianRed: '#FF4500',
+    //   martianSoilOrane: '#FFA500',
+    //   cosmicBlue: '#000080',
+    //   neutral: '#F5F5F5',
+    // },
   },
   plugins: [],
 }
