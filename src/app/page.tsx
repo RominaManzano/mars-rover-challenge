0e1becd78e2b8549247ce74f99d2ccba12da5b4e
Romina Manzano <manzano.romina13@gmail.com>
2023-07-14 21:23:01 -0300

    FIX: removed unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import classnames from 'classnames';
-
 import RoverCard from '@/components/RoverCard';
 import { Rover } from '@/types/Rover.type';
 
 const Home: React.FC = () => {
-  // const backgroundClasses = 'bg-gradient-to-br from-black via-black to-orange-900';
   const generalClasses = 'flex min-h-screen flex-col md:flex-row items-center justify-between p-24 gap-8';
 
   return (
-    <main className={classnames(generalClasses)}>
+    <main className={generalClasses}>
       <RoverCard name={Rover.CURIOSITY} />
       <RoverCard name={Rover.OPPORTUNITY} />
       <RoverCard name={Rover.SPIRIT} />
