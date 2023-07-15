3c86530b0737919b809ee3c16f7802f4e13d8e94
Romina Manzano <manzano.romina13@gmail.com>
2023-07-14 23:19:36 -0300

    UPDATE: home page ui improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import RoverCard from '@/components/RoverCard';
 import { Rover } from '@/types/Rover.type';
 
 const Home: React.FC = () => {
-  const generalClasses = 'flex h-full flex-col md:flex-row items-center justify-between p-24 gap-8';
+  const generalClasses = 'flex h-full flex-col md:flex-row items-center justify-between p-12 lg:p-24 gap-8';
 
   return (
     <div className="min-h-screen flex flex-col justify-center">
-      <div className="m-10 flex flex-col items-center justify-center">
-        <h1 className="text-5xl uppercase font-extrabold">Mars Rover Challenge</h1>
-        <h3 className="text-lg mt-4 italic text-gray-400">Select a Rover to see their photos</h3>
+      <div className="m-8 md:m-10 flex flex-col items-center justify-center">
+        <h1 className="text-2xl md:text-5xl uppercase font-extrabold text-center">
+          Mars Rover Challenge
+        </h1>
+        <h3 className="text-md md:text-lg mt-4 italic text-gray-400 text-center">
+          Select a Rover to see their photos
+        </h3>
       </div>
 
       <main className={generalClasses}>
--- src/components/RoverCard.tsx
+++ src/components/RoverCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import classnames from 'classnames';
 
 import { Rover } from '@/types/Rover.type';
 
@@ -8,25 +9,29 @@ interface RoverCardProps {
   name: Rover;
 };
 
-const RoverCard: React.FC<RoverCardProps> = ({ name }) => (
-  <div className="rounded-2xl overflow-hidden">
-    <Link href={`/${name}`}>
-      <div className="relative">
-        <Image
-          src={`/images/${name}-rover.jpg`}
-          alt={name}
-          width={320}
-          height={320}
-          className="opacity-80"
-        />
-        <div className="w-full flex justify-center items-center absolute z-10 top-[50%]">
-          <span className="text-xl capitalize">
-            {name}
-          </span>
+const RoverCard: React.FC<RoverCardProps> = ({ name }) => {
+  const positionClasses = 'w-full h-full flex justify-center items-center absolute z-10 top-0 hover:cursor-pointer';
+  const backgroundClasses = 'bg-black bg-opacity-30 hover:bg-opacity-10';
+
+  return (
+    <div className="rounded-2xl overflow-hidden drop-shadow-lg">
+      <Link href={`/${name}`}>
+        <div className="relative">
+          <Image
+            src={`/images/${name}-rover.jpg`}
+            alt={name}
+            width={320}
+            height={320}
+          />
+          <div className={classnames(positionClasses, backgroundClasses)}>
+            <h4 className="text-lg uppercase font-bold tracking-[0.2rem] drop-shadow-lg">
+              {name}
+            </h4>
+          </div>
         </div>
-      </div>
-    </Link>
-  </div>
-);
+      </Link>
+    </div>
+  );
+};
 
 export default RoverCard
